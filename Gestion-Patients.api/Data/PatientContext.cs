7c454fabfaa1ed5caec032b0dd34019b75b75243
Axek <axel.phanor64@gmail.com>
2024-06-27 13:37:52 +0200

    feat: spécifications des relations de la base de données gestion-patients

--- Gestion-Patients.api/Data/PatientContext.cs
+++ Gestion-Patients.api/Data/PatientContext.cs
@@ -8,6 +8,41 @@ namespace Gestion_Patients.api.Data
     {
         public PatientContext(DbContextOptions<PatientContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gender>()
+                .HasIndex(g => g.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Gender>()
+                .HasMany(g => g.Patients)
+                .WithOne(g => g.Gender)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Address>()
+                .HasIndex(address => address.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Address>()
+                .HasMany(g => g.Patients)
+                .WithOne(g => g.Address)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Patient>()
+                .HasOne(p => p.Gender)
+                .WithMany(g => g.Patients)
+                .HasForeignKey(p => p.GenderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Patient>()
+                .HasOne(p => p.Address)
+                .WithMany(a => a.Patients)
+                .HasForeignKey(p => p.AddressId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Gender> Genders { get; set; }
         public DbSet<Address> Addresses { get; set; }
