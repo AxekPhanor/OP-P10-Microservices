0dce69e05cd16e88d4b1bcb660af5a7e5c38f6a6
Axek <axel.phanor64@gmail.com>
2024-07-08 16:31:27 +0200

    refactor(client): suppression d'import inutile

--- Angular-App.client/src/app/pages/page-gestion-patients/page-gestion-patients.component.ts
+++ Angular-App.client/src/app/pages/page-gestion-patients/page-gestion-patients.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { GestionPatientsService } from '../../services/gestion-patients.service';
 import { PatientOutput } from '../../models/patientOutput';
 import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
-
 
 @Component({
   selector: 'app-page-gestion-patients',
